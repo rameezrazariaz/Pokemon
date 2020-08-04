cbda4eef5cda57d6e408c2f67a17daab67624290
Rameez Raza Riaz <rameezrazariaz@gmail.com>
2020-08-04 18:46:33 +0530

    added more functionality

--- pokemon.py
+++ pokemon.py
@@ -1,11 +1,11 @@
 class Pokemon:
-    def __init__(self, name, level, type, knocked_out):
+    def __init__(self, name, level, type):
         self.name = name
         self.level = level
         self.type = type
         self.max_health = 100
         self.current_health = self.max_health
-        self.knocked_out = knocked_out
+        self.knocked_out = False
 
     def __repr__(self):
         return "{} is at level {} of type {} with current health {}.\n".format(
@@ -18,11 +18,12 @@ class Pokemon:
             self.is_knocked_out()
         else:
             print(
-                "{} has lost {} health. It now has {} health left.\n".format(self.name, health_lost, self.current_health))
+                "{} has lost {} health. It now has {} health left.\n".format(self.name, health_lost,
+                                                                             self.current_health))
 
     def gain_health(self, heal):
         if self.current_health <= 0:
-            self.revive
+            self.revive()
         health_gained = heal
         self.current_health += health_gained
         if self.current_health > self.max_health:
@@ -30,9 +31,9 @@ class Pokemon:
         print("{} has gained {} health. It now has {} health.\n".format(self.name, health_gained, self.current_health))
 
     def revive(self):
-        self.knocked_out == False
-        self.current_health = self.max_health
-        print("{} was revived. It now has {} health.\n".format(self.name, self.current_health))
+        if self.knocked_out:
+            self.current_health = self.max_health
+            print("{} was revived. It now has {} health.\n".format(self.name, self.current_health))
 
     def is_knocked_out(self):
         if self.current_health <= 0:
@@ -76,7 +77,6 @@ class Trainer:
         return "{} has {} potions with {} currently active.\n".format(
             self.name, self.potions, self.current_pokemon.name)
 
-
     def use_potion(self):
         if self.potions > 0:
             if self.current_pokemon.current_health < self.current_pokemon.max_health:
@@ -85,7 +85,8 @@ class Trainer:
                 self.potions -= 1
                 print("{} has {} potions left.\n".format(self.name, self.potions))
             else:
-                print("Failed to use potion on {} as it is already at maximum health.\n".format(self.current_pokemon.name))
+                print("Failed to use potion on {} as it is already at maximum health.\n".format(
+                    self.current_pokemon.name))
         else:
             print("{} has no potions left.\n".format(self.name))
 
@@ -95,24 +96,71 @@ class Trainer:
         my_pokemon.attack(opponent_pokemon)
 
     def switch_pokemon(self, pokemon):
-        if self.pokemons[pokemon].knocked_out:
+        if pokemon.knocked_out:
             print("Pokemon is knocked out. Cannot switch to specified Pokemon.\n")
         else:
             self.current_pokemon = pokemon
-            print("Switched pokemon to {}.\n".format(self.pokemons[pokemon].name))
+            print("Switched pokemon to {}.\n".format(pokemon.name))
 
 
 # The game
-pikachu = Pokemon("Pikachu", 3, "Fire", False)
-bulbasaur = Pokemon("Bulbasaur", 3, "Grass", False)
-squirtle = Pokemon("Squirtle", 3, "Water", False)
+pikachu = Pokemon("Pikachu", 3, "Fire")
+bulbasaur = Pokemon("Bulbasaur", 3, "Grass")
+squirtle = Pokemon("Squirtle", 3, "Water")
 
-trainer1 = Trainer('Trainer1', [pikachu], 2, pikachu)
-trainer2 = Trainer('Trainer2', [bulbasaur, squirtle], 2, bulbasaur)
+trainer1_name = input("Enter Your name: ")
+trainer1_pokemons_no = int(input("Enter the number of pokemons you have: "))
+trainer1_pokemons = []
 
-print(pikachu)
-print(bulbasaur)
-print(squirtle)
+for i in range(trainer1_pokemons_no):
+    name = input("Enter the name of Pokemon no " + str(i + 1) + ": ")
+    level = int(input("Enter the level of " + name + ": "))
+    type = input("Enter the type of " + name + ": ")
+    print()
+
+    new_pokemon = Pokemon(name.title(), level, type)
+    trainer1_pokemons.append(new_pokemon)
+
+# trainer1_potions = 2
+if trainer1_pokemons_no > 1:
+    trainer1_pokemon_choice = input("Enter the pokemon you want to use: ")
+    for i in trainer1_pokemons:
+        if i.name == trainer1_pokemon_choice.title():
+            trainer1_currently_active = i
+else:
+    trainer1_currently_active = trainer1_pokemons[0]
+
+trainer2_name = input("Enter Your name: ")
+trainer2_pokemons_no = int(input("Enter the number of pokemons you have: "))
+trainer2_pokemons = []
+
+for i in range(trainer2_pokemons_no):
+    name = input("Enter the name of Pokemon no " + str(i + 1) + ": ")
+    level = int(input("Enter the level of " + name + ": "))
+    type = input("Enter the type of " + name + ": ")
+    print()
+
+    new_pokemon2 = Pokemon(name.title(), level, type)
+    trainer2_pokemons.append(new_pokemon2)
+
+# trainer2_potions = 2
+if trainer2_pokemons_no > 1:
+    trainer2_pokemon_choice = input("Enter the pokemon you want to use: ")
+    for i in trainer2_pokemons:
+        if i.name == trainer2_pokemon_choice.title():
+            trainer2_currently_active = i
+else:
+    trainer2_currently_active = trainer2_pokemons[0]
+
+print(trainer1_currently_active)
+print(trainer2_currently_active)
+
+trainer1 = Trainer(trainer1_name, trainer1_pokemons, 2, trainer1_currently_active)
+trainer2 = Trainer(trainer2_name, trainer2_pokemons, 2, trainer2_currently_active)
+
+# print(pikachu)
+# print(bulbasaur)
+# print(squirtle)
 
 print(trainer1)
 print(trainer2)
@@ -120,7 +168,8 @@ print(trainer2)
 
 trainer1.attack_other_trainer(trainer2)
 trainer2.attack_other_trainer(trainer1)
-trainer2.use_potion()
 trainer1.attack_other_trainer(trainer2)
-trainer2.switch_pokemon(0)
-trainer2.switch_pokemon(1)
+trainer2.use_potion()
+# trainer2.switch_pokemon(trainer2.pokemons[1])
+trainer1.attack_other_trainer(trainer2)
+trainer2.attack_other_trainer(trainer1)
